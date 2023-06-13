d1b0dfe03a1fa5e555f671438553590fef59d5ca
Leo Rochard <leo.rochard@rca.fr>
2023-06-13 16:06:10 +0200

    Maj: graph.py

--- Projet/graph.py
+++ Projet/graph.py
@@ -1,19 +1,20 @@
 import random
 import time
+import heapq
 
 
 class Graph:
-    def __init__(self, num_nodes):
+    def __init__(self, num_nodes, max_edge):
         self.graph = dict()
-        self.generate_graph(num_nodes)
+        self.generate_graph(num_nodes, max_edge)
 
-    def add_edge(self, node1, node2, cost):
+    def add_edge(self, node1, node2, cost, max_edge):
         if node1 not in self.graph:
             self.graph[node1] = dict()
         if node2 not in self.graph:
             self.graph[node2] = dict()
 
-        if node1 != node2:
+        if node1 != node2 and len(self.graph[node1]) < max_edge:
             self.graph[node1][node2] = int(cost)
             self.graph[node2][node1] = int(cost)
 
@@ -24,25 +25,93 @@ class Graph:
                 return True, self.graph[city1][city2]
         return False, None
 
-    def generate_graph(self, node_count):
+    def generate_graph(self, node_count, max_edge):
         print("---------------------------------------------------------")
         print(f"Generating Graph with {node_count} nodes")
 
-        num_edges = node_count * (node_count - 1) // 2  # Nombre d'arêtes pour un graphe connexe
-
         tic = time.perf_counter()
+        num_edges = node_count * (node_count - 1) // 2  # Nombre d'arêtes restantes pour un graphe connexe
+
         for _ in range(num_edges):
-            node1 = random.randint(0, node_count - 1)  # Générer un identifiant de nœud aléatoire
+            node1 = random.randint(0, node_count - 1)
             node2 = random.randint(0, node_count - 1)
             cost = random.randint(10, 100)  # Coût aléatoire entre 10 et 100 (minutes)
-            self.add_edge(node1, node2, cost)
+            self.add_edge(node1, node2, cost, max_edge)
+
         toc = time.perf_counter()
         print(f"Generation done in {toc - tic:0.4f} seconds")
         print("---------------------------------------------------------")
 
+        def generate_graph(self, node_count, max_edges_per_node):
+            print("---------------------------------------------------------")
+            print(f"Generating Graph with {node_count} nodes")
+
+            tic = time.perf_counter()
+
+            available_nodes = set(range(node_count))
+            selected_nodes = set()
+
+            start_node = random.choice(tuple(available_nodes))
+            selected_nodes.add(start_node)
+            available_nodes.remove(start_node)
+
+            while available_nodes:
+                node = random.choice(tuple(selected_nodes))
+                num_edges = min(max_edges_per_node, len(available_nodes))
+                if num_edges == 0:
+                    break
+
+                for _ in range(num_edges):
+                    adjacent_node = random.choice(tuple(available_nodes))
+                    cost = random.randint(10, 100)  # Coût aléatoire entre 10 et 100 (minutes)
+                    self.add_edge(node, adjacent_node, cost)
+                    selected_nodes.add(adjacent_node)
+                    available_nodes.remove(adjacent_node)
+
+            toc = time.perf_counter()
+            print(f"Generation done in {toc - tic:0.4f} seconds")
+            print("---------------------------------------------------------")
+
     def print_graph(self):
         for key in self.graph:
             print(f"{key} --> {self.graph[key]}")
+
+    def dijkstra(self, start, end):
+        distances = {node: float('inf') for node in self.graph}
+        distances[start] = 0
+
+        heap = [(0, start)]
+        while heap:
+            current_cost, current_node = heapq.heappop(heap)
+
+            if current_node == end:
+                break
+
+            if current_cost > distances[current_node]:
+                continue
+
+            for neighbor, cost in self.graph[current_node].items():
+                new_cost = current_cost + cost
+                if new_cost < distances[neighbor]:
+                    distances[neighbor] = new_cost
+                    heapq.heappush(heap, (new_cost, neighbor))
+
+        if distances[end] == float('inf'):
+            return None, None
+
+        path = self.reconstruct_path(start, end, distances)
+        return path, distances[end]
+
+    def reconstruct_path(self, start, end, distances):
+        current_node = end
+        path = [current_node]
+        while current_node != start:
+            neighbors = self.graph[current_node]
+            min_neighbor = min(neighbors, key=lambda node: distances[node] + neighbors[node])
+            path.append(min_neighbor)
+            current_node = min_neighbor
+        path.reverse()
+        return path
 
 
 """
--- Projet/main.py
+++ Projet/main.py
@@ -3,6 +3,19 @@ import graph as g
 
 if __name__ == '__main__':
     # Exemple d'utilisation
-    g = g.Graph(2000)
-    print(g.graph)
-    print(len(g.graph))
+    g = g.Graph(2000, 5)
+    #g.print_graph()
+
+    start_node = 1
+    end_node = 1578
+
+    path, cost = g.dijkstra(start_node, end_node)
+    if path is None:
+        print("No path found.")
+    else:
+        print(f"Shortest Path from {start_node} to {end_node} :", path)
+        print("Cost:", cost)
+
+
+
+
